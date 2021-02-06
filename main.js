6586995069f600e56451abff795e96c6b68def9c
berselis <68135098+berselis@users.noreply.github.com>
2021-02-06 16:15:52 -0400

    Deploy
    Limpiando espacios

--- main.js
+++ main.js
@@ -2,18 +2,12 @@ let diametro = window.prompt("Inserte el valor del diametro");
 let grosor = window.prompt("Inserte el valor del grosor");
 
 if(diametro > 1.4){
-    console.log("la rueda es para un vehiculo grande")
+    console.log("la rueda es para un vehiculo grande");
 }else if(diametro <= 1.4 && diametro > 0.8){
     console.log("La rueda es para un vehiculo mediano");
-
 }else{
-    console.log("La rueda es para un vehiculo pequeño")
+    console.log("La rueda es para un vehiculo pequeño");
 }
-
-
 if ((diametro > 1.4 && grosor < 0.4) || (diametro <= 1.4 && diametro > 0.8 && grosor < 0.25)){
- console.log("El grosor para esta rueda es inerior al recomendado")
+ console.log("El grosor para esta rueda es inerior al recomendado");
 }
-
-
-
