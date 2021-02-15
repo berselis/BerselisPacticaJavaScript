0d4311dc6105c992f36f48bb4b78da815133ba10
berselis <68135098+berselis@users.noreply.github.com>
2021-02-15 13:37:01 -0400

    Deploy
    Tarea formulario y manipulacion de objeto

---
+++ Tarea1/fuctionScript.js
@@ -0,0 +1,21 @@
+const Formulario = document.getElementById('Formulario');
+
+Formulario.addEventListener("submit", AplicarCambios);
+
+const AplicarCambios = function(event){
+    event.preventDefault();
+
+    let tamanio = event.target.SizeLetra.value;
+    let color = event.target.SetColor.value;
+    let ancho = event.target.SizeAncho.value;
+    let alto = event.target.SizeAlto.value;
+
+    const objeto = document.getElementById('objeto');
+    objeto.style.transition = '0.9s';
+    objeto.style.fontSize = tamanio + 'px';
+    objeto.style.backgroundColor = color;
+    objeto.style.width = ancho + 'px';
+    objeto.style.height = alto + 'px';
+    
+}
+
