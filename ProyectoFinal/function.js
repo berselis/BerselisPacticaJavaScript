1f1433d02bb6b328cd0db12966d2077958928878
berselis <68135098+berselis@users.noreply.github.com>
2021-03-20 13:31:20 -0400

    Deploy
    Bug fixed

--- ProyectoFinal/function.js
+++ ProyectoFinal/function.js
@@ -3,6 +3,7 @@ const SetInfoForm = function (formulario) {
     let titulo = formulario.target.titulo.value;
     let detalle = formulario.target.descripcion.value;
 
+
     listItems.append(AddNewItems(titulo, detalle));
 
     formulario.target.titulo.value = null;
@@ -10,25 +11,70 @@ const SetInfoForm = function (formulario) {
 
     formulario.target.titulo.focus();
 }
+const ChangeStatus = function (inputCheck) {
 
+
+    const element = inputCheck.path[2];
+    if (inputCheck.target.checked == true) {
+        inputCheck.path[3].lastChild.value = true;
+        element.classList.remove('inner-tittleRaw');
+        element.classList.add('inner-tittleAct');
+    } else {
+        inputCheck.path[3].lastChild.value = false;
+        element.classList.remove('inner-tittleAct');
+        element.classList.add('inner-tittleRaw');
+    }
+}
+const DeleteItems = function (closeButton) {
+
+    const element = closeButton.path[1];
+    let closeValue = closeButton.target.value;
+
+    if (closeValue == 'false') {
+        listItems.removeChild(element);
+    }
+}
 const listItems = document.getElementById('listIntems');
 const myForm = document.getElementById("myForm");
-
 myForm.addEventListener('submit', SetInfoForm);
 
-function AddNewItems(t, d) {
-    const item = document.createElement('li');
-    item.className = 'list-group-item';
+function AddNewItems(titulo, detalle) {
 
-    const titulo = document.createElement('p');
-    titulo.className = 'lead';
-    titulo.innerHTML = t;
+    const divInner = document.createElement('div');
+    divInner.classList.add('form-check', 'form-switch');
 
-    const detalle = document.createElement('p');
-    detalle.innerHTML = d;
+    const inputCheck = document.createElement('input');
+    inputCheck.className = 'form-check-input';
+    inputCheck.type = 'checkbox';
+    inputCheck.addEventListener('click', ChangeStatus);
 
-    item.appendChild(titulo);
-    item.appendChild(detalle);
+    const label = document.createElement('label');
+    label.className = 'form-check-label';
+    label.innerText = titulo;
 
-    return item;
+    divInner.appendChild(inputCheck);
+    divInner.appendChild(label);
+
+    const divOuter = document.createElement('div');
+    divOuter.classList.add('col-md-12', 'inner-tittleRaw');
+    divOuter.appendChild(divInner);
+
+    const p = document.createElement('p');
+    p.className = 'listDetail';
+    p.innerText = detalle;
+
+    const buttonClose = document.createElement('button');
+    buttonClose.type = 'button';
+    buttonClose.classList.add('btn-close', 'position-absolute', 'top-0', 'end-0');
+    buttonClose.setAttribute('aria-label', 'Close');
+    buttonClose.value = false;
+    buttonClose.addEventListener('click', DeleteItems);
+
+    const li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.appendChild(divOuter);
+    li.appendChild(p);
+    li.appendChild(buttonClose);
+
+    return li;
 }
